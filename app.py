1881778c9d6500f3f5da9ade734f8994b6d9aa26
Andrew Liu <liu.andrew1988@gmail.com>
2021-04-08 16:43:34 -0400

    {Resource: post.py} create post.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flask_migrate import Migrate
 from models.db import db
 from sqlalchemy.orm import joinedload
+from models.post import Post
+from resources.post import SinglePost, Posts
 
 
 app = Flask(__name__)
@@ -14,6 +16,10 @@ api.config['SQLALCHEMY_ECHO'] = True
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+api.add_resource(Posts, '/posts')
+api.add_resource(SinglePost, '/posts/<int:id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ resources/post.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource
+from flask import request
+from models.post import Post
+
+
+class Posts(Resource):
+    def get(self):
+        post = Post.find_all()
+        return post
+
+    def post(self):
+        post = Post(**request.get_json())
+        post.create()
+        return post.json(), 201
+
+
+class SinglePost(Resource):
+    def get(self, id):
+        post = Post.find_by_id(id)
+        return post.json()
+
+    def delete(self, id):
+        post = Post.delete(id)
+        return {'msg': 'Post Deleted', 'payload': post['id']}
