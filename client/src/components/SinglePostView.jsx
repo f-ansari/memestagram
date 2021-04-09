2521d9e42896e9300ce911b2f057c51cfd434899
Faryal Ansari <faryalansari@live.com>
2021-04-08 21:49:43 -0400

    new route and onclick

--- client/src/components/SinglePostView.jsx
+++ client/src/components/SinglePostView.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import { GetOnePost } from '../store/actions/PostAction'
 
 const SinglePostView = (props) => {
+    console.log(props.match.params.id)
     return (
         <div>
             <h1>post details</h1>
