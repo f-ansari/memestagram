f20600576e2a3d5c55f6a2dfebcccf94373cb258
Faryal Ansari <faryalansari@live.com>
2021-04-08 20:33:04 -0400

    creating form

--- client/src/store/reducers/PostReducers.js
+++ client/src/store/reducers/PostReducers.js
@@ -13,24 +13,24 @@ const PostReducer = (state = iState, action) => {
   switch (action.type) {
     case GET_POST:
       return { ...state, mapPost: action.payload }
-    // case CREATE_POST:
-    //   return {
-    //     ...state,
-    //     createPost: {
-    //       ...state.createPost,
-    //       [action.paylaod.name]: action.payload.value
-    //     }
-    //   }
-    // case ADD_POST:
-    //   return {
-    //     ...state,
-    //     mapPost: [...state.mapPost, { name: state.createPost }]
-    //   }
-    // case DELETE_POST:
-    //   const mapPost = state.mapPost.filter(
-    //     (destroyPost, i) => destroyPost.id !== action.paylaod.data.item
-    //   )
-    //   return { ...state, ...mapPost }
+    case CREATE_POST:
+      return {
+        ...state,
+        createPost: {
+          ...state.createPost,
+          [action.paylaod.name]: action.payload.value
+        }
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        mapPost: [...state.mapPost, { name: state.createPost }]
+      }
+    case DELETE_POST:
+      const mapPost = state.mapPost.filter(
+        (destroyPost, i) => destroyPost.id !== action.paylaod.data.item
+      )
+      return { ...state, ...mapPost }
     default:
       return { ...state }
   }
